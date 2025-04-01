157abf81473120bd3b0ac2b2f1c2bc29c965b69d
Nikita <niktos@icloud.com>
2025-04-01 21:22:44 +0300

    add hooks useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,48 @@
-import TaskForm from "./components/TaskForm";
-import TaskList from "./components/TaskList";
+import TaskForm from "./components/Main/TaskForm";
+import TaskList from "./components/Main/ComplitedTask";
+import ComplitedTask from "./components/Main/ComplitedTask";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { useState } from "react";
 
 function App() {
+
+  const[openSection, setOpensection] = useState({
+    taskList: false,
+    tasks: true,
+    complitedTask: true
+  })
+
+  function toogleSection(section) {
+    setOpensection((prev) => ({
+      ...prev,
+      [section]: !prev[section]
+    }))
+  }
+
   return (
     <div className="app">
+      <Header />
       <div className="task-container">
         <h1>Task List</h1>
-        <button className="close-button">+</button>
-        <TaskForm />
+        <button className={`close-button ${openSection.taskList ? "open" : ""}`} onClick={() => toogleSection("taskList")}>+</button>
+        {openSection.taskList && <TaskForm />}
       </div>
       <div className="completed-task-container">
         <h2>Task`s</h2>
-        <button className="close-button">+</button>
+        <button className={`close-button ${openSection.tasks ? "open" : ""}`} onClick={() => toogleSection("tasks")}>+</button>
         <div className="sort-controls">
           <button className="sort-button">By date</button>
           <button className="sort-button">By priority</button>
         </div>
-        <TaskList />
+        {openSection.tasks && <TaskList />}
       </div>
       <div className="task-container">
         <h2>Completed Task`s</h2>
-        <button className="close-button">+</button>
+        <button className={`close-button ${openSection.complitedTask ? "open" : ""}`} onClick={() => toogleSection("complitedTask")}>+</button>
+        {openSection.complitedTask && <ComplitedTask />}
       </div>
+      <Footer />
     </div>
   );
 }
