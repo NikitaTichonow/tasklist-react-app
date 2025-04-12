c2883d7f81f0098d0e21a70b5474baae4583d6ae
Nikita <niktos@icloud.com>
2025-04-12 07:39:30 +0300

    update TaskForm.jsx

--- src/components/Main/TaskForm.jsx
+++ src/components/Main/TaskForm.jsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
+
 function TaskForm() {
-    return <form action="" className="task-form">
-        <input type="text" value={""} placeholder="task title" required/>
-        <select value={""}>
-            <option value={"High"}>High</option>
-            <option value={"Medium"}>Medium</option>
-            <option value={"Low"}>Low</option>
-        </select>
-        <input type="datetime-local" required value={""}/>
-        <button type="submit">Add task</button>
+  const [title, setTitle] = useState("");
+  const [priority, setPriority] = useState("Low");
+  const [deadline, setDeadline] = useState("");
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+  }
+
+  return (
+    <form action="" className="task-form">
+      <input
+        type="text"
+        value={title}
+        placeholder="task title"
+        required
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+        <option value={"High"}>High</option>
+        <option value={"Medium"}>Medium</option>
+        <option value={"Low"}>Low</option>
+      </select>
+      <input
+        type="datetime-local"
+        required
+        value={deadline}
+        onChange={(e) => setDeadline(e.target.value)}
+      />
+      <button type="submit">Add task</button>
     </form>
+  );
 }
 
-export default TaskForm
+export default TaskForm;
